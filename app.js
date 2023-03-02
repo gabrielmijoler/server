eba01edc40744d517ae68ebf1623da1261299d88
gabrielmijoler <gabrielmijoler@hotmail.com>
2023-03-02 16:42:56 -0300

    feat: mogoose e mongoDB configurando

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql');
 const app = express()
+const port = process.env.PORT || 4000
 
+
+// mongodb+srv://gabrielsmijoler:<password>@graphqlcluster.pxte4z4.mongodb.net/?retryWrites=true&w=majority
 
 const schema = require('./schema/schema')
+const testschema = require('./schema/types_schema')
+const mongoose = require("mongoose")
+
 
 app.use(
     '/graphql',
     graphqlHTTP({
       graphiql: true,
-      schema: schema
+      schema: testschema
     }),
   );
   
-app.listen(4000,  ()=>{
-    console.log('listening for requests 3000')
-})
+mongoose.connect(`mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@graphqlcluster.pxte4z4.mongodb.net/${process.env.mongoDataBase}?retryWrites=true&w=majority`,
+{userNewUrlParser: true, useUnifieldTopologgy: true, useCreateIndex: true}
+).then(() => {
+  app.listen(port,  ()=>{
+      console.log('listening for requests' + port)
+  })
+}).catch((e) => console.log("Error::" + e))
--- schema/types_schema.js
+++ schema/types_schema.js
@@ -6,21 +6,59 @@ const {
     GraphQLObjectType,
     GraphQLID,
     GraphQLString,
+    GraphQLBoolean,
+    GraphQLFloat,
     GraphQLInt,
-    GraphQLList
+    GraphQLNonNull,
+    GraphQLList,
 } = graphql
 
+//Scalar types
 
+
+const Person = new GraphQLObjectType({
+    name: 'Person',
+    description: 'Represents a Person Type',
+    fields:() => ({
+        id:{type: GraphQLID},
+        name:{type: new GraphQLNonNull(GraphQLString)},
+        age:{type:new GraphQLNonNull(GraphQLInt)},
+        isMarried:{type: GraphQLBoolean},
+        gpa:{type: GraphQLFloat},
+        
+        jusAType:{
+            type: Person,
+            resolve(parent, args) {
+                return parent;
+            }
+        }
+    })
+})
+
+// RootQuery
 const RootQuery = new GraphQLObjectType({
     name: 'RootQuery',
     description: 'Descreption',
-    fields: {}
+    fields: {
+        person:{
+            type: Person,
+            resolve(parent, args) {
+                let personObj = {
+                    name:'Antonio',
+                    age:34,
+                    isMarried: true,
+                    gpa: 4.0,
+                }
+                return personObj;
+            }
+        }
+    }
 });
+
 
 
 
 
 module.exports = new GraphQLSchema({
     query: RootQuery,
-    mutation: Mutation
 })
